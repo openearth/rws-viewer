14f5bedc56ef1269af89679ffc4f12a3828bc1e9
Peter Goes <petergoes@gmail.com>
2021-11-23 17:49:33 +0100

    feat(i18n): Add i18n

---
+++ src/plugins/i18n.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+import nl from '../../translations/nl.json'
+
+Vue.use(VueI18n)
+
+export const defaultLocale = 'nl'
+export const availableLocales = [ 'nl', 'en' ]
+
+export const i18n = new VueI18n({
+  locale: defaultLocale,
+  messages: { nl },
+})
