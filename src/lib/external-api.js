d1ab1d0fd9bef26446d6057b788597a89a88a3f9
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2022-07-04 14:58:24 +0200

    move download of file to function

--- src/lib/external-api.js
+++ src/lib/external-api.js
@@ -6,3 +6,22 @@ export const generateDownloadUrl = ({
 
   return `${ url }?filter=${ formattedFilter };&pagesize=10000&format=csv`
 }
+
+export const downloadFromUrl = ({ url, apiKey }) => {
+  const options = {
+    headers: {
+      ...(apiKey ? { 'x-api-key': apiKey } : {}),
+    },
+  }
+
+  return fetch(url, options)
+    .then(res => res.blob() )
+    .then(blob => {
+      var file = window.URL.createObjectURL(blob)
+
+      window.location.assign(file)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
