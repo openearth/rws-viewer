52ad73681a825095364197f2481e47768787673a
IoannaMi1 <ioannamicha@hotmail.com>
2022-10-18 11:17:11 +0200

    add configuration object for each viewer (mapZoom, mapCenter); add translations

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,16 +1,10 @@
-export const VALID_VIEWER_CONFIGS = require('../../public/data/available-configs.json')
-console.log(VALID_VIEWER_CONFIGS)
+export const VALID_VIEWER_CONFIGS = require('../../public/data/available-configs-viewers.json')
+console.log('VALID_VIEWER_CONFIGS',VALID_VIEWER_CONFIGS )
+export const VALID_VIEWER_NAMES =  VALID_VIEWER_CONFIGS.map(({ name }) => name)
 
 export const CATEGORIES = require('../../public/data/app.json').categories
 
 export const COORDINATES_HANDLE = '[[COORDINATES]]'
-
-
-export const NEDERLANDS_MAP_CENTER = [ 5.2913, 52.1326 ] // LONG, LAT
-export const WADDEN_SEA_MAP_CENTER = [ 5.787, 53.198 ]
-
-export const WADDEN_SEA_MAP_ZOOM = 8.5
-export const NEDERLANDAS_MAP_ZOOM = 7
 
 // Layer types
 export const OWS_LAYER_TYPE = 'ows'
--- src/repo/configRepo.js
+++ src/repo/configRepo.js
@@ -1,20 +1,27 @@
-import { VALID_VIEWER_CONFIGS } from '~/lib/constants'
+import { VALID_VIEWER_CONFIGS, VALID_VIEWER_NAMES } from '~/lib/constants'
 import axios from 'axios'
+
+
+
+export function getViewerConfiguration(viewer) {
+  const viewerConfiguration = VALID_VIEWER_CONFIGS.find(config => config.name === viewer)
+  return viewerConfiguration
+}
 
 async function getViewerData(viewer) {
   const fileName = `${ viewer }.json`
   const { data } = await axios(`/data/${ fileName }`)
   return data
 }
-
+ 
 const configRepo = {
   async getConfig(viewers) {
     const validViewers = viewers
       .split(',')
-      .filter(viewer => VALID_VIEWER_CONFIGS.includes(viewer))
+      .filter(viewer => VALID_VIEWER_NAMES.includes(viewer))
 
     if (validViewers.length === 0) {
-      const viewerToUse = VALID_VIEWER_CONFIGS[0]
+      const viewerToUse = VALID_VIEWER_NAMES[0]
       validViewers.push(viewerToUse)
       console.warn(`No (valid) viewer provided, falling back to ${ viewerToUse.name }`)
     }
