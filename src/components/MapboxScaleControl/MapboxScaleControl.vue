77f71092c3d08b11211f456015cd80e120bcfc8e
Lucia Sarni <lucia@voorhoede.nl>
2023-06-01 12:21:32 +0200

    Add a Scale Control to the map

---
+++ src/components/MapboxScaleControl/MapboxScaleControl.vue
@@ -0,0 +1,20 @@
+<script>
+  import Mapbox from 'mapbox-gl'
+
+  export default {
+    methods: {
+      deferredMountedTo(map) {
+        window.map = map
+
+        const mbScale = new Mapbox.ScaleControl({
+          maxWidth: 100,
+          unit: 'metric',
+        })
+
+        map.addControl(mbScale, 'top-right')
+      },
+    },
+
+    render: () => null,
+  }
+</script>
