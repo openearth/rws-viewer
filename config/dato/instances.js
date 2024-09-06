41aa54295e51af06b26f6f591f1e9297f2bc3968
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2024-09-06 15:25:12 +0200

    temp disable instances

--- config/dato/instances.js
+++ config/dato/instances.js
@@ -7,12 +7,12 @@ module.exports.instances = [
     name: "nl2120",
     key: process.env.DATO_API_KEY_NL2120,
   },
-  {
-    name: "openearth-data-viewer",
-    key: process.env.DATO_API_KEY_OPENEARTH_DATA_VIEWER,
-  },
-  {
-    name: "openearth-rws-viewer",
-    key: process.env.DATO_API_KEY_OPENEARTH_RWS_VIEWER,
-  },
+  // {
+  //   name: "openearth-data-viewer",
+  //   key: process.env.DATO_API_KEY_OPENEARTH_DATA_VIEWER,
+  // },
+  // {
+  //   name: "openearth-rws-viewer",
+  //   key: process.env.DATO_API_KEY_OPENEARTH_RWS_VIEWER,
+  // },
 ];
