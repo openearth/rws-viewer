04246ddf4d4cb287760b743e48b3b28876de3709
luisrodriguezgalvez <luis.rodriguez@deltares.nl>
2025-02-12 17:56:57 +0100

    cleaned up component

--- src/components/ScreenshotButton/ScreenshotButton.vue
+++ src/components/ScreenshotButton/ScreenshotButton.vue
@@ -1,25 +1,22 @@
 <template>
   <v-tooltip bottom max-width="200px">
     <template v-slot:activator="{ on }">
-      <v-btn
-        class="ma-auto"
+      <v-btn class="ma-auto"
         v-on="on"
         icon
         @click="snapShot"
       >
-        <v-icon> mdi-content-save </v-icon>
+        <v-icon>mdi-content-save</v-icon>
       </v-btn>
     </template>
-    <span>
-      {{ $t('screenshot') }}
-    </span>
+    <span>{{ $t('screenshot') }}</span>
   </v-tooltip>
 </template>
 
 <script>
   export default {
     props: {
-      map: Object // Accept the map instance as a prop
+      map: Object
     },
     methods: {
       snapShot() {
@@ -28,12 +25,8 @@
           return;
         }
       
-        // Ensure that the latest frame is fully rendered
         this.map.once('render', () => {
-          const mapCanvas = this.map.getCanvas();
-          const mapImage = mapCanvas.toDataURL("image/png");
-
-          // Create a downloadable link
+          const mapImage = this.map.getCanvas().toDataURL("image/png");
           const link = document.createElement("a");
           link.href = mapImage;
           link.download = "map_snapshot.png";
@@ -41,8 +34,7 @@
           link.click();
           document.body.removeChild(link);
         });
-
-        // Force the map to render the latest frame
+      
         this.map.triggerRepaint();
       }
     }
