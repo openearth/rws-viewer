f1b9ae8d33af303e2d3f9a619650742c0689ba37
Sjoerd Beentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2025-03-11 17:46:03 +0100

    update api proxy so the url can be variable (#151)
    * update api proxy so the url can be variable
    
    * update strategy of getting urls
    
    * Add API URLs to GitHub Actions workflows and refactor datocms utility functions
    
    * fix env errors
    
    * add functions to netlify config
    
    * Add functions-dir to Netlify workflows
    
    * Update build process to copy Netlify functions to dist
    
    * add functions to netlify config
    
    * Refactor build and function configuration for Netlify deployment
    
    * test functions strategy
    
    * add some logging
    
    * update redirect
    
    * adjust file format
    
    * Refactor API proxy function, improve error handling and logging
    
    * Remove development logging and simplify data handling
    
    * update response
    
    * fix proxy
    
    * fix proxy
    
    * add logging
    
    * simplify
    
    * simplify even more
    
    * fix headers
    
    * fix

--- config/dato/instances.js
+++ config/dato/instances.js
@@ -1,21 +1,28 @@
-const dotenv = require("dotenv");
+// If the API keys are not set, load them from the .env file
+if (!process.env.DATO_API_KEY_NL2120) {
+  const dotenv = require("dotenv");
 
-dotenv.config();
+  dotenv.config();
+}
+
 
 module.exports.instances = [
   {
     name: "nl2120",
     datoApiKey: process.env.DATO_API_KEY_NL2120,
     netlifySiteId: "1f6372f6-c532-4e0e-bba7-dee08678d518",
+    apiUrl: process.env.API_URL_NL2120,
   },
   {
     name: "openearth-data-viewer",
     datoApiKey: process.env.DATO_API_KEY_OPENEARTH_DATA_VIEWER,
     netlifySiteId: "1785f3f6-b4cf-42de-bea6-b57d48a5c664",
+    apiUrl: process.env.API_URL_OPENEARTH_DATA_VIEWER,
   },
   {
     name: "openearth-rws-viewer",
     datoApiKey: process.env.DATO_API_KEY_OPENEARTH_RWS_VIEWER,
     netlifySiteId: "119b8ff3-5b22-4995-b43b-b31f21ba77c3",
+    apiUrl: process.env.API_URL_OPENEARTH_RWS_VIEWER,
   },
 ];
