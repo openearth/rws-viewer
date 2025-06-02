4a56166637b617c1b805d0673b55955cf24e0b6c
IoannaMi <ioannamicha@hotmail.com>
2025-06-02 18:08:16 +0200

    fix errors out of bounds

--- src/lib/build-wmts-layer.js
+++ src/lib/build-wmts-layer.js
@@ -1,27 +1,18 @@
-import { max } from 'date-fns'
 import buildGeoserverUrl from './build-geoserver-url'
 
 const defaultUrl = process.env.VUE_APP_GEOSERVER_BASE_URL
 
-export default ({ url: rawUrl = defaultUrl, id, layer, style = '', paint = {}, version = '1.0.0' }) => {
-  //TODO:'https://datahuiswadden.openearth.nl/geoserver/gwc/service/
-  // wmts?SERVICE=WMTS&
-  // REQUEST=GetTile&
-  // VERSION=1.0.0&
-  // LAYER=terpen:terp_dem_20210308_cog&
-  // STYLE=&
-  // TILEMATRIXSET=EPSG:900913&TILEMATRIX=EPSG:900913:{z}&
-  // TILEROW={y}&TILECOL={x}&FORMAT=image/png' 
+export default ({ url: rawUrl = defaultUrl, id, layer, style = '', paint = {}, version = '1.0.0', bbox = [] }) => {
+
 
   const url = new URL(rawUrl)
- 
   const tile = buildGeoserverUrl({
     url: url.origin + url.pathname,
     service: 'WMTS',
     request: 'GetTile',
     layer: layer, 
     style,
-    version, //either 1.0.0 or pass it from the capabilities
+    version, 
     format: 'image/png',
     tilematrixset: 'EPSG:900913',
     tilematrix: 'EPSG:900913:{z}',
@@ -38,6 +29,8 @@ export default ({ url: rawUrl = defaultUrl, id, layer, style = '', paint = {}, v
       type: 'raster',
       tiles: [ tile ],
       tileSize: 256,
+      bounds: bbox
+
     },
     paint,
 
