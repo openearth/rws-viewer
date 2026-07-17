3aa4f863f747610525947c3ba003b6c370030f2e
Mares2022 <mario.fuentesmonjaraz@deltares.nl>
2026-07-17 14:39:53 +0200

    Fixing GetLegendGraphic input url for WMTS layers
    GetLegendGraphic is a WMS-only operation and is not supported by WMTS. When a layer uses WMTS, its url is translated to the equivalent WMS endpoint so GetLegendGraphic can be used

--- src/lib/build-legend-url.js
+++ src/lib/build-legend-url.js
@@ -1,9 +1,20 @@
 import { stringify } from 'query-string'
+import checkMapServiceType from './check-map-service-type'
 
 // GeoServer - GetLegendGraphic Docs
 // https://docs.geoserver.org/stable/en/user/services/wms/get_legend_graphic/index.html
 export default function (layerData) {
   const { url, layer } = layerData
+
+  // GetLegendGraphic is a WMS operation. If the layer's url points at the
+  // WMTS/GWC endpoint, swap it for the equivalent WMS endpoint of the layer's workspace.
+  const isWmts = checkMapServiceType(url) === 'wmts'
+  const workspace = layer.includes(':') ? layer.split(':')[0] : null
+  const legendBaseUrl = isWmts && workspace
+    ? url.replace('/gwc/service/wmts', `/${ workspace }/wms`)
+    : url
+
+  // Set the query params for the GetLegendGraphic request
   const params = stringify({
     'request': 'GetLegendGraphic',
     'service':'wms',
@@ -13,5 +24,5 @@ export default function (layerData) {
     'legend_options': 'fontAntiAliasing:true;fontColor:0x000000;fontSize:16;labelMargin:8;dpi:90;',
   })
 
-  return `${ url }?${ params }`
+  return `${ legendBaseUrl }?${ params }`
 }
